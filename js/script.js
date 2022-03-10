a1800324dd87d0f9bec688ddeb05b1b1dde27812
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-10 18:16:45 +0000

    Added math and fixed readme

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-// Cristiano | ICS2O-Unit0-00-HTML | Date 2022
+// Cristiano | ICS2O-Unit2-02-HTML | March 10 2022
 
 
-/**
- * Alerts "Hello, World!"
- */
-function onButtonClick() {
-  alert("Hello, World!")
+/*
+  Does basic math.
+*/
+function doMathQuestions() {
+  document.getElementById("math-addition").innerHTML = "<p>5 + 3 = " + (5 + 3) + "</p>"
+  document.getElementById("math-subtraction").innerHTML = "<p>5 - 3 = " + (5 - 3) + "</p>"
+  document.getElementById("math-multiplication").innerHTML = "<p>5 × 3 = " + (5 * 3) + "</p>"
+  document.getElementById("math-division").innerHTML = "<p>5 ÷ 2.5 = " + (5 / 2.5) + "</p>"
+  document.getElementById("math-powers").innerHTML = "<p>5 + 3² = " + (5 + 3 ** 2) + "</p>"
 }
